f9b0ad9a99a179d9ef536b701067b9a4a955fa60
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2014-05-08 09:26:20 +0200

    A big change in svscalib

--- calibration/svscalib.py
+++ calibration/svscalib.py
@@ -5,8 +5,7 @@ from flexvi.opencv import calibration
 from flexvi.opencv import stereovision as sv
 from flexvi.calibration.containers.svs import StereoVisionSystem
 from flexvi.opencv.images import get_image_size
-
-DEFAULT_FINDCBC = chessboard.flags['at_or_fq']
+from flexvi.dataanalysis import sampling
 
 def compute_intrinsics(images_left, images_right, corners_left, corners_right, pattern_size, square_size):
     intrinsics_left = calibration.calibrate_camera(images_left, pattern_size, square_size, corners_left)[1:3]    
@@ -15,21 +14,77 @@ def compute_intrinsics(images_left, images_right, corners_left, corners_right, p
 
 class StereoVisionSystemCalibrator:
     
-    def __init__(self, imagemasks, pattern_size, square_size, indices=None, findcbc_flags=None):
-        if findcbc_flags is None:
-            findcbc_flags = DEFAULT_FINDCBC
+    def __init__(self, imset1, imset2, sample_size=10, nsamples=5):
             
-        self.pattern_size = pattern_size
-        self.square_size = square_size            
+        self.imset1 = imset1
+        self.imset2 = imset2
         
-        imc = chessboard.open_images_and_find_corners_universal(imagemasks[0], pattern_size, imagemasks[1], findcbc_flags, indices)
-        self.cres1, self.cres2, self.images1, self.images2 = imc[:-2]
-    
+        self.pattern_size = imset1.pattern_size
+        self.square_size = imset2.square_size
+
+        self._compute_best_intrinsics(sample_size, nsamples)            
+        
+        
+    def _compute_best_intrinsics(self, sample_size, nsamples):
+        
+        ''' 1. Generate samples '''
+        nimages = len(self.imset1.image_files)
+        samples = sampling.generate_list_of_samples(nimages, sample_size, nsamples)
+        #print 'Samples: %s' % samples
+        
+        ''' 
+        2. Make a set union among all the samples so that only the needed
+        images are opened further
+        '''
+        all_samples_set = set(samples[0])
+        for i in range(1, nsamples):
+            all_samples_set.union(samples[i])
+        all_indices = list(all_samples_set)
+            
+        ''' 3. Open images and find chessboard corners '''
+        imc = chessboard.open_images_and_find_corners_universal(self.imset1.imagemask, self.pattern_size, self.imset2.imagemask, self.imset1.findcbc_flags, all_indices)
+        cres1, cres2, images1, images2 = imc[:-2]        
+        
+        ''' 
+        4. For each of the samples conduct camera calibration only for 
+        the first camera
+        '''
+        calib1_results = []
+        indices_list = []
+        for s in samples:
+            
+            indices = [all_indices.index(i) for i in s]
+            indices_list.append(indices)            
+            
+            im1 = [images1[ind] for ind in indices]
+            c1 = [cres1[ind] for ind in indices]
+            
+            res = calibration.calibrate_camera(im1, self.pattern_size, self.square_size, c1)
+            calib1_results.append(res)
+            
+        '''
+        5. Find the sample number that leads to the smallest error of
+        camera calibration (RMS)
+        '''
+        min_error_index = 0    
+        for i in range(1, nsamples):
+            if calib1_results[i][0] < calib1_results[min_error_index][0]:
+                min_error_index = i
+              
+        ''' 
+        6. Select the intirnsics results (camera 1) for the best calibration
+        and compute intrinsics for the second camera using the same 
+        images sample
+        '''
+        self.rms = calib1_results[min_error_index][0]
+        self.intrinsics1 = calib1_results[min_error_index][1:3]
+        
+        im2 = [images2[ind] for ind in indices_list(min_error_index)]
+        c2 = [cres2[ind] for ind in indices_list(min_error_index)]
+        self.intrinsics2 = calibration.calibrate_camera(im2, self.pattern_size, self.square_size, c2)[1:3]
+            
     def calibrate(self):   
         
-        lr_intrinsics = compute_intrinsics(self.images1, self.images2, self.cres1, self.cres2, self.pattern_size, self.square_size)    
-        self.intrinsics1, self.intrinsics2 = lr_intrinsics
-                    
         self.svs = StereoVisionSystem()    
         res = sv.calibrate_stereo_vision_system(self.images1, self.images2, self.pattern_size, self.square_size, self.intrinsics1, self.intrinsics2, self.cres1, self.cres2)
         self.rms = res[0]        
